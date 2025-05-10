758f6627cadacd0e08fbb9ad86eb5a0c047a392c
James Powell <blazzike@gmail.com>
2025-05-10 12:03:23 +0100

    implement all instructions!

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import type {Instruction} from "./types.ts";
 import {compileInstruction} from "./helpers.ts";
 
-let rom = await Bun.file('./chip8.rom').bytes();
+let rom = await Bun.file('./octojam6title.ch8').bytes();
 let memory = new Uint8Array(4096);
 let regV = new Uint8Array(16);
 
@@ -10,6 +10,8 @@ memory.set(rom, kProgramStart);
 
 let regPc = kProgramStart;
 let regI = 0;
+
+const regStack = new Uint16Array(16);
 let regSp = 0;
 let regDt = 0;
 let regSt = 0;
@@ -25,11 +27,26 @@ class Display {
   }
 
   public draw(sprite: Uint8Array, offsetX: number, offsetY: number) {
+    let somePixelsErased = false;
     for (let y = 0; y < sprite.length; y++) {
-      this.buffer.set(sprite[y].toString(2).padStart(8, '0').split('').map((v) => v === '1' ? 1 : 0), ((y + offsetY) * this.kWidth) + offsetX);
+      for (let x = 0; x < 8; x++) {
+        const bit = (sprite[y] >> (7 - x)) & 1; // Notice the change here
+        // if (bit === 0) {
+        //   continue;
+        // }
+
+        let bufferIndex = ((y + offsetY) * this.kWidth) + offsetX + x;
+        if (this.buffer[bufferIndex] === 1 && bit === 1) {
+          somePixelsErased = true;
+        }
+
+        this.buffer[bufferIndex] ^= bit;
+      }
     }
 
     display.print();
+
+    return somePixelsErased;
   }
 
   public print() {
@@ -49,51 +66,139 @@ const instructionSet: Instruction[] = [
   compileInstruction('00E0', () => {
     display.clear();
   }),
-  // 00EE
-  // 0nnn
+  compileInstruction('00EE', () => {
+    regSp--;
+    if (regSp < 0) {
+      throw new Error('Stack underflow');
+    }
+
+    regPc = regStack[regSp];
+  }),
+  compileInstruction('0nnn', () => {
+    // ignored:
+    // Jump to a machine code routine at nnn.
+    //
+    // This instruction is only used on the old computers on which Chip-8 was originally implemented. It is ignored by modern interpreters.
+  }),
   compileInstruction('1nnn', ({nnn}) => {
     regPc = nnn - 2;
   }),
-  // 2nnn
-  // 3xkk
-  // 4xkk
-  // 5xy0
+  compileInstruction('2nnn', ({nnn}) => {
+    regStack[regSp] = regPc;
+    regSp++;
+    if (regSp > 15) {
+      throw new Error('Stack overflow');
+    }
+
+    regPc = nnn - 2;
+  }),
+  compileInstruction('3xkk', ({x, kk}) => {
+    if (regV[x] === kk) {
+      regPc += 2;
+    }
+  }),
+  compileInstruction('4xkk', ({x, kk}) => {
+    if (regV[x] !== kk) {
+      regPc += 2;
+    }
+  }),
+  compileInstruction('5xy0', ({x, y}) => {
+    if (regV[x] === regV[y]) {
+      regPc += 2;
+    }
+  }),
   compileInstruction('6xkk', ({x, kk}) => {
     regV[x] = kk;
   }),
   compileInstruction('7xkk', ({x, kk}) => {
-    regV[x] = regV[x] + kk;
+    regV[x] = (regV[x] + kk) & 0xFF;
   }),
-  // 8xy0
-  // 8xy1
-  // 8xy2
-  // 8xy3
-  // 8xy4
-  // 8xy5
-  // 8xy6
-  // 8xy7
-  // 8xyE
-  // 9xy0
+  compileInstruction('8xy0', ({x, y}) => {
+    regV[x] = regV[y];
+  }),
+  compileInstruction('8xy1', ({x, y}) => {
+    regV[x] = regV[x] | regV[y];
+  }),
+  compileInstruction('8xy2', ({x, y}) => {
+    regV[x] = regV[x] & regV[y];
+  }),
+  compileInstruction('8xy3', ({x, y}) => {
+    regV[x] = regV[x] ^ regV[y];
+  }),
+  compileInstruction('8xy4', ({x, y}) => {
+    const sum = regV[x] + regV[y];
+    regV[0xF] = sum > 255 ? 1 : 0;
+    regV[x] = sum & 0xFF;
+  }),
+  compileInstruction('8xy5', ({x, y}) => {
+    const diff = regV[x] - regV[y];
+    regV[0xF] = diff < 0 ? 0 : 1;
+    regV[x] = diff & 0xFF;
+  }),
+  compileInstruction('8xy6', ({x, y}) => {
+    regV[0xF] = regV[x] & 0x1;
+    regV[x] >>= 1;
+  }),
+  compileInstruction('8xy7', ({x, y}) => {
+    regV[0xF] = regV[y] >= regV[x] ? 1 : 0;
+    regV[x] = (regV[y] - regV[x]) & 0xFF;
+  }),
+  compileInstruction('8xyE', ({x, y}) => {
+    regV[0xF] = regV[x] >> 7;
+    regV[x] <<= 1;
+  }),
+  compileInstruction('9xy0', ({x, y}) => {
+    if (regV[x] !== regV[y]) {
+      regPc += 2;
+    }
+  }),
   compileInstruction('Annn', ({nnn}) => {
     regI = nnn;
   }),
-  // Bnnn
+  compileInstruction('Bnnn', ({nnn}) => {
+    regPc = (nnn + regV[0]) - 2;
+  }),
   compileInstruction('Cxkk', ({x, kk}) => {
     regV[x] = (Math.random() * 256) & kk;
   }),
   compileInstruction('Dxyn', ({x, y, n}) => {
     const bytes = memory.slice(regI, regI + n);
-    display.draw(bytes, regV[x], regV[y]);
+    const posX = regV[x] % 64; // Ensure wrapping using modulo, CHIP-8 screen width is 64
+    const posY = regV[y] % 32; // CHIP-8 screen height is 32
+
+    const somePixelsErased = display.draw(bytes, posX, posY);
+    regV[0xF] = somePixelsErased ? 1 : 0;
   }),
-  // Ex9E
-  // ExA1
-  // Fx07
+  compileInstruction('Ex9E', ({x}) => {
+    // TOOD:
+    // Skip next instruction if key with the value of Vx is pressed.
+    // Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
+
+    // for now, let's just do nothing
+  }),
+  compileInstruction('ExA1', ({x}) => {
+    // TODO:
+    // Skip next instruction if key with the value of Vx is not pressed.
+    // Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
+
+    // for now, let's just always skip
+    regPc += 2;
+  }),
+  compileInstruction('Fx07', ({x}) => {
+    regV[x] = regDt;
+  }),
   compileInstruction('Fx0A', ({x}) => {
     while (true) {} // TODO
   }),
-  // Fx15
-  // Fx18
-  // Fx1E
+  compileInstruction('Fx15', ({x}) => {
+    regDt = regV[x];
+  }),
+  compileInstruction('Fx18', ({x}) => {
+    regSt = regV[x];
+  }),
+  compileInstruction('Fx1E', ({x}) => {
+    regI = regI + regV[x];
+  }),
   compileInstruction('Fx29', ({x}) => {
     const digit = regV[x];
     regI = digit * 5;
@@ -108,9 +213,15 @@ const instructionSet: Instruction[] = [
     memory[regI + 1] = tens; // Store tens place at memory[regI + 1]
     memory[regI + 2] = ones; // Store ones place at memory[regI + 2]
   }),
-  // Fx55
+  compileInstruction('Fx55', ({x}) => {
+    for (let i = 0; i <= x; i++) {
+      memory[regI + i] = regV[i];
+    }
+  }),
   compileInstruction('Fx65', ({x}) => {
-    regV.set(memory.subarray(regI + regV[0], regI + x), 0);
+    for (let i = 0; i <= x; i++) {
+      regV[i] = memory[regI + i];
+    }
   }),
 ];
 
@@ -137,14 +248,14 @@ for (let i = 0; i < spriteChars.length; i++) {
   memory.set(spriteChars[i], i * 5);
 }
 
-while (true) {
-  let opcode = (memory[regPc] << 8) | memory[regPc + 1];
+function executeInstruction() {
+  const opcode = (memory[regPc] << 8) | memory[regPc + 1];
   let hasMatched = false;
+  // console.debug(opcode.toString(16).padStart(4, '0'));
   for (const instr of instructionSet) {
     if ((opcode & instr.mask) === instr.value) {
       instr.handler(opcode);
       hasMatched = true;
-
       break;
     }
   }
@@ -155,3 +266,22 @@ while (true) {
 
   regPc += 2;
 }
+
+const instructionRate = 3000; // default: 540 Hz
+
+let cycleCount = 0;
+let startTime = Date.now();
+while (true) {
+  executeInstruction();
+  cycleCount++;
+  let targetCycleCount = 0;
+  while (cycleCount > targetCycleCount) {
+    targetCycleCount = Math.floor(((Date.now() - startTime) / 1000) * instructionRate);
+  }
+}
+
+const timerRate = 60;
+const timerInterval = 1000 / timerRate;
+setInterval(() => {
+  if (regDt > 0) regDt--;
+}, timerInterval);
